708bb0c8c9a28c21d304522b1eebcc5784e17e5f
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-06 12:02:20 +0200

    implemented repository.

---
+++ HelpingHand.Core/Data/Common/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace HelpingHand.Core.Data.Common
+{
+    public interface IRepository
+    {
+        Task SaveChangesAsync();
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        IQueryable<T> GetOne<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        IQueryable<T> GetAll<T>() where T : class;
+    }
+}
---
+++ HelpingHand.Core/Data/Common/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HelpingHand.Core.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private DbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+             DbSet<T>().Remove(entity);
+        }
+
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> GetOne<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return GetAll<T>().Where(predicate);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        private DbSet<T> DbSet<T>()
+            where T : class
+        {
+            return _context.Set<T>();
+        }
+    }
+}
